1c78a91109888ad10dc7637dac58e1b8e67959bf
chandkaurati <chandkaurati@outlook.com>
2024-03-21 11:30:44 +0530

    test

--- script.js
+++ script.js
@@ -3,4 +3,7 @@ const  buttonEl = document.getElementById('btn')
 buttonEl.addEventListener('click',(e)=>{
      e.preventDefault()
      alert('welcome to shetkari mitra your end destination to get end to end knowledge  about morden agriculture')
+     let div = document.createElement('div')
+     div.innerHTML = 'welcome to shetkari mitra your end destination to get end to end knowledge  about morden agriculturewelcome to shetkari mitra your end destination to get end to end knowledge  about morden agriculture'
+     document.getElementsByTagName('body').append(div)
 })
