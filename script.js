f457d167acc7c0c1eaf2e38df046505382aaff96
chandkaurati <chandkaurati@outlook.com>
2024-03-21 11:25:51 +0530

    added the starter fiels

---
+++ script.js
@@ -0,0 +1,6 @@
+const  buttonEl = document.getElementById('btn')
+
+buttonEl.addEventListener('click',(e)=>{
+     e.preventDefault()
+     alert('welcome to shetkari mitra your end destination to get end to end knowledge  about morden agriculture')
+})
